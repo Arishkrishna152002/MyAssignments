b6f8dd98d0873e6c32e3437f1d774e7e88e49f67
Arishkrishna152002 <160860102+Arishkrishna152002@users.noreply.github.com>
2024-02-22 17:59:42 +0530

    Add files via upload
    DAY-1--->creating class and objects.

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+package week1Day1;
+
+public class Vehicle {
+	public static void bajajPulsor() {
+		System.out.println("bajajPulsor");
+
+	}
+
+	public static void hondaShine() {
+		System.out.println("hondaShine");
+	}
+
+	public static void main(String[] args) {
+		Vehicle obj = new Vehicle();
+		obj.bajajPulsor();
+		obj.hondaShine();
+	}
+}
