f2d965a902fb98c0fc48cae0ca2e7cbd019a9b67
Arishkrishna152002 <160860102+Arishkrishna152002@users.noreply.github.com>
2024-02-26 16:24:22 +0530

    Add files via upload
    SWAPPING TWO NUMBERS

---
+++ SwappingTwoNumbers.java
@@ -0,0 +1,75 @@
+//SWAPPING TWO NUMBERS WITHOUT USING TEMPORARY VARIABLES
+/*package week2.Day2;
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=80;
+		
+		
+		//b=b-a//100-50 b=50
+		//a=a+b//50+50  a=100
+		
+		b=b-a;//b-50
+		a=b+a;//a=100
+		
+		
+		System.out.println("the value of A is:"+a);
+		System.out.println("the value of B is:"+b);
+		
+	}
+
+}
+SWAPPING TWO NUMBERS USING TEMPORARY VARIABLE
+
+package week2.Day2;
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=80;
+		int temp;
+		
+		temp=a;             //using temporary variable
+		a=b;
+		b=temp;
+		
+		
+		System.out.println("the value of A is:"+a);
+		System.out.println("the value of B is:"+b);
+		
+	}
+
+}*/
+//SWAPPING TWO VARIABLE BY CREATING A METHOD
+/*package week2.Day2;
+
+public class SwappingTwoNumbers {
+	
+	
+	public void usingTemp() {
+		 
+			int a=50;
+			int b=100;
+			int temp;
+			
+		
+		
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+	public static void main(String[] args) {
+		
+		SwappingTwoNumbers sw = new SwappingTwoNumbers();
+		sw.usingTemp();
+		
+		
+	}
+}*/
+
