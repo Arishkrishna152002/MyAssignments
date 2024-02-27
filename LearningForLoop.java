90ff0260a89348d1f16de515e4ece2432f61e8df
Arishkrishna152002 <160860102+Arishkrishna152002@users.noreply.github.com>
2024-02-27 19:25:03 +0530

    Add files via upload
    For Loops

---
+++ LearningForLoop.java
@@ -0,0 +1,29 @@
+package week2.Day2;
+
+public class LearningForLoop {
+
+	public static void main(String[] args) {
+		
+		/*for(initialization;condition;increment or decrement){
+		  }*/
+		/*for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}*/
+	/////////finding odd numbsers from 1 to 10////////////////	
+		/*for(int i=1;i<10;i++) {
+			int sum;
+			sum=2*i+1;
+			System.out.println(sum);
+				
+			}*/
+		for(int i=1;i<10;i++) {
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
+		}
+		
+
+	}
+
+
